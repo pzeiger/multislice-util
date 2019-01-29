b5931d1592abc856a115e8aabbd29ab9144e7480
Paul Zeiger <paul.zeiger@uni-konstanz.de>
2019-01-29 14:11:51 +0100

    working incoherent and coherent averaging

--- avg_diffpatt_over_snaps/incohavg_diffpatt_over_snaps.pl
+++ avg_diffpatt_over_snaps/incohavg_diffpatt_over_snaps.pl
@@ -42,7 +42,9 @@ foreach my $i ( 0 .. $#ARGV ) {
 
 
 my @avgint = ();
-my @avgsqint = ();
+#my @avgsqint = ();
+my @M = ();
+my @S = ();
 
 
 for my $j ( 0 .. $#filein ) {
@@ -73,39 +75,54 @@ for my $j ( 0 .. $#filein ) {
 	foreach my $index ( 0 .. $#inp ) {
 		my $ix = $inp[$index][1];
 		my $iy = $inp[$index][2];
+		my $int;
 		
 		if ( $setshift == 1 ) {
 			$ix = (( $ix - 1 + ($ixmax/2) ) % $ixmax) + 1;
 			$iy = (( $iy - 1 + ($iymax/2) ) % $iymax) + 1;
 		}
-			$avgint[$ix][$iy] += $inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4];
-			$avgint[$ix][$iy] += $inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4];
-			$avgsqint[$ix][$iy] += ($inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4])**2;
-			$avgsqint[$ix][$iy] += ($inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4])**2;
+		$int = $inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4];
+		$avgint[$ix][$iy] += $int;
+		
+#		$avgsqint[$ix][$iy] += ($inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4])**2;
+#		$avgsqint[$ix][$iy] += ($inp[$index][3]*$inp[$index][3] + $inp[$index][4]*$inp[$index][4])**2;
+		
+		# compute running stddev according to Knuth's algorithm (https://www.johndcook.com/blog/standard_deviation/)
+		if ($j == 0) {
+			$M[$ix][$iy][1] = $int;
+			$S[$ix][$iy][1] = .0;
+		} else {
+			$M[$ix][$iy][2] = $M[$ix][$iy][1] + ($int - $M[$ix][$iy][1]) / ($j+1);
+			$S[$ix][$iy][2] = $S[$ix][$iy][1] + ($int - $M[$ix][$iy][1]) * ($int - $M[$ix][$iy][2]);
+			$M[$ix][$iy][1] = $M[$ix][$iy][2];
+			$S[$ix][$iy][1] = $S[$ix][$iy][2];
+		}
 	}
 	
 	foreach my $n ( @outevery ) {
 		if ( (( $j + 1 ) % $n) == 0 or $j == $#filein ) {
-			my @stddev = ();
-			my @uncavgint = ();
+			my $stddev;
+			my $uncert;
 			my $out;
-			for my $ix  ( 1 .. $#avgint ) {
-				for my $iy ( 1 .. $#{$avgint[$ix]} ) {
-					$stddev[$ix][$iy] += sqrt(1/$j * ($avgsqint[$ix][$iy] - $avgint[$ix][$iy]**2/($j+1)));
-					$uncavgint[$ix][$iy] += sqrt(1/($j+1)) * $stddev[$ix][$iy];
-				}
-			}
+#			for my $ix  ( 1 .. $#avgint ) {
+#				for my $iy ( 1 .. $#{$avgint[$ix]} ) {
+#					$stddev[$ix][$iy] += sqrt(1/$j * abs(($avgsqint[$ix][$iy] - $avgint[$ix][$iy]**2/($j+1))));
+#					$uncavgint[$ix][$iy] += sqrt(1/($j+1)) * $stddev[$ix][$iy];
+#				}
+#			}
 			if ( $j == $#filein ) {
-				my $FILEOUT = "cohavg_diffpatt" . ($#filein + 1) . "final";
+				my $FILEOUT = "incohavg_diffpatt" . ($#filein + 1) . "final";
 				open $out, '>:encoding(UTF-8)', $FILEOUT;
 			} else {
-				my $FILEOUT = "cohavg_diffpatt" . ($j+1);
+				my $FILEOUT = "incohavg_diffpatt" . ($j+1);
 				open $out, '>:encoding(UTF-8)', $FILEOUT;
 			}
 			
-			for my $ix ( 1 .. $#sumamp ) {
-				for my $iy ( 1 .. $#{$sumamp[$ix]} ) {
-					printf $out "%4i %4i %1.12e %1.12e\n", ($ix, $iy, $avgint[$ix][$iy]/($j+1), $stddev[$ix][$iy], $uncavgint[$ix][$iy]);
+			for my $ix ( 1 .. $#avgint ) {
+				for my $iy ( 1 .. $#{$avgint[$ix]} ) {
+					$stddev = sqrt($S[$ix][$iy][1]/$j);
+					$uncert = sqrt(1/($j+1)) * $stddev;
+					printf $out "%4i %4i %1.12e %1.12e %1.12e\n", ($ix, $iy, $avgint[$ix][$iy]/($j+1), $stddev, $uncert);
 				}
 				printf $out "\n";
 			}
